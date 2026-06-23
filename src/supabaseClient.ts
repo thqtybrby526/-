016b7c6c2ef54c06b5983742e0f7ea9aa5712973
thqtybrby526 <thqtybrby526@gmail.com>
2026-06-23 17:43:50 +0300

    Delete src directory

--- src/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// ضع رابطك الحقيقي ومفتاحك هنا مباشرة بدون أي كلمات VITE_
-const supabaseUrl = "https://wufymhbyxwheihtxadnb.supabase.co"; 
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1ZnltaGJ5eHdoZWlodHhhZG5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NTA2NDksImV4cCI6MjA5NzAyNjY0OX0.15VZe17WklPQ1_aAoxk8yWOJUvlHt4dTdx5P7vALgHg"; 
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
